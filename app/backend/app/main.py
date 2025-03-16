744a189726c9f74d6f72095db96264655f64d968
Kazik <kazik@example.com>
2025-03-16 14:39:04 +0000

    last

--- app/backend/app/main.py
+++ app/backend/app/main.py
@@ -1,5 +1,5 @@
 
-from models import ComputerParts, Storage_response, CPU_response, Memory_response, Motherboard, Cooler, GPU_response, PSU, Case
+from models import ComputerParts, Storage_response, CPU_response, Memory_response, Motherboard_response, Cooler_response, GPU_response, PSU_response, Case_response
 
 import sqlalchemy as db
 from sqlalchemy import select
@@ -89,7 +89,7 @@ def cpu(build: Build):
     }
 
 
-@app.post("/memory", response_model=ComputerParts)
+@app.post("/ram", response_model=ComputerParts)
 def memory(build: Build):
     memory_query = select(RAM)
 
@@ -97,8 +97,9 @@ def memory(build: Build):
         cpu_query = select(CPU).where(CPU.sku == build.cpu)
         cpu = session.scalars(cpu_query).one_or_none()
         if cpu:
+            name = cpu.name.split(' ')[0]
             memory_query = memory_query.where(
-                (RAM.XMP_support == build.XMP_support) | (RAM.AMDexpo_support == build.AMDexpo_support)
+                RAM.AMDexpo_support == 1
             )
 
     if build.mb:
@@ -114,12 +115,12 @@ def memory(build: Build):
     final_list = [Memory_response(
         name = ram.name,
         price = ram.price,
-        SKU = ram.price,
+        SKU = ram.sku,
         image_url = ram.image_url,
         link = ram.link,
         ram_type = ram.ram_type,
-        ram_slots = ram.ram_slots,
-        memory = ram.memory,
+        ram_slots = int(ram.ram_slots.split(' ')[0]),
+        memory = int(ram.memory.split(' ')[0]),
         XMP_support = ram.XMP_support,
         AMDexpo_support = ram.AMDexpo_support
     ) for ram in rams]
@@ -161,12 +162,83 @@ def disk(build: Build):
 
 @app.post("/mb", response_model=ComputerParts)
 def mb(build: Build):
-    return {"name": "motherboards", "data": []}
+    mb_query = select(Motherboard)
+
+    if build.case:
+        case_query = select(Case).where(Case.sku == build.case)
+        case = session.scalars(case_query).one_or_none()
+        if case:
+            mb_query = mb_query.where(
+                Motherboard.case_compatibility == case.case_format
+            )
+    
+    if build.cpu:
+        cpu_query = select(CPU).where(CPU.sku == build.cpu)
+        cpu = session.scalars(cpu_query).one_or_none()
+        if cpu:
+            mb_query = mb_query.where(
+                Motherboard.socket == cpu.socket
+            )
+
+    if build.ram:
+        ram_query = select(RAM).where(RAM.sku == build.ram)
+        ram = session.scalars(ram_query).one_or_none()
+        if ram:
+            mb_query = mb_query.where(
+                (Motherboard.ram_slots >= ram.ram_slots) | (Motherboard.max_ram >= ram.memory) | (Motherboard.ram_type == ram.ram_type)
+            )
+    
+    if build.gpu:
+        gpu_query = select(GraphicCard).where(GraphicCard.sku == build.gpu)
+        gpu = session.scalars(gpu_query).one_or_none()
+        if gpu:
+            if gpu.pcie_version == 5:
+                mb_query = mb_query.where(
+                    Motherboard.pcie5_slots > 0
+                )
+            elif gpu.pcie_version == 4:
+                mb_query = mb_query.where(
+                    (Motherboard.pcie4_x16 > 0) | (Motherboard.pcie4_x4 > 0)  
+                )
+
+    mbs = session.scalars(mb_query).all()
+
+    final_list = [Motherboard_response(
+        name = mb.name,
+        price = mb.price,
+        SKU = mb.sku,
+        image_url = mb.image_url,
+        link = mb.link,
+        case_compatibility = mb.case_compatibility,
+        socket = mb.socket,
+        ram_slots = mb.ram_slots,
+        max_ram = int(mb.max_ram.split(' ')[0]),
+        ram_type = mb.ram_type,
+        pcie5_slots = mb.pcie5_slots,
+        pcie4_x16 = mb.pcie4_x16,
+        pcie4_x4 = mb.pcie4_x4
+    ) for mb in mbs]
+    
+
+    return {"message": "motherboards", "data": final_list}
 
 
 @app.post("/cooler", response_model=ComputerParts)
-def cooler():
-    return {"name": "coolers", "data": []}
+def cooler(build: Build):
+    cooling_query = select(Cooling)
+    coolers = session.scalars(cooling_query).all()
+
+    final_list = [Cooler_response(
+        name = cool.name,
+        price = cool.price,
+        SKU = cool.sku,
+        image_url = cool.image_url,
+        link = cool.link,
+        nr_fans = cool.number_of_fans,
+        fan_diameter = int(cool.fan_diamater.split(' ')[0]),
+        power_consumption = cool.power_consumption
+    ) for cool in coolers]
+    return {"message": "coolers", "data": final_list}
 
 
 @app.post("/gpu", response_model=ComputerParts)
@@ -212,11 +284,79 @@ def gpu(build: Build):
 
 
 @app.post("/psu", response_model=ComputerParts)
-def psu():
-    return {"name": "psus", "data": []}
+def psu(build: Build):
+    psu_query = select(PSU)
+
+    if build.gpu:
+        gpu_query = select(GraphicCard).where(GraphicCard.sku == build.gpu)
+        gpu = session.scalars(gpu_query).one_or_none()
+        if gpu:
+            psu_query = psu_query.where(
+                int(PSU.power_consumption.split(' ')[0]) >= gpu.power_consumption
+            )
+    
+    if build.cpu:
+        cpu_query = select(CPU).where(CPU.sku == build.gpu)
+        cpu = session.scalars(cpu_query).one_or_none()
+        if cpu:
+            psu_query = psu_query.where(
+                int(PSU.power_consumption.split(' ')[0]) >= int(cpu.power_consumption.split(' ')[0])
+            )
+    
+    psus = session.scalars(psu_query).all()
+
+    final_list = [PSU_response(
+        name = psu.name,
+        price = psu.price,
+        link = psu.link,
+        SKU = psu.sku,
+        image_url = psu.image_url,
+        depth = psu.depth,
+        power = int(psu.power.split(' ')[0])
+    ) for psu in psus]
+
+
+    return {"message": "psus", "data": final_list}
 
 
 @app.post("/case", response_model=ComputerParts)
-def case():
-    return {"name": "cases", "data": []}
+def case(build: Build):
+    case_query = select(Case)
+
+    if build.mb:
+        mb_query = select(Motherboard).where(Motherboard.sku == sku)
+        mb = session.scalars(mb_query).one_or_none()
+        if mb:
+            case_query = case_query.where(
+                Case.case_format == mb.case_compatibility
+            )
+
+    if build.gpu:
+        gpu_query = select(GraphicCard).where(GraphicCard.sku == build.gpu)
+        gpu = session.scalars(gpu_query).one_or_none()
+        if gpu:
+            case_query = case_query.where(
+                Case.max_gpu_size >= gpu.length
+            )
+    
+    if build.psu:
+        psu_query = select(PSU).where(PSU.sku == build.sku)
+        psu = session.scalars(psu_query).one_or_none()
+        if psu:
+            case_query = case_query.where(
+                (Case.max_psu_length >= psu.depth)
+            )
+    
+    cases = session.scalars(case_query).all()
+
+    final_list = [Case_response(
+        name = case.name,
+        price = case.price, 
+        SKU = case.sku, 
+        image_url = case.image_url,
+        link = case.link,
+        case_format = case.case_format,
+    ) for case in cases]
+
+    return {"message": "cases", "data": final_list}
 
